3d32a3508b042187bf0b4fbe2aae861ccfd7efa5
udaa001 <calm.udaa001@gmail.com>
2025-01-12 22:44:11 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 global.bankname = "MONIEPOINT"
 
-global.banknumber = "8133729715"
+global.banknumber = "9065996326"
 
-global.bankowner = "FADARE"
+global.bankowner = "Muhammad Ali"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
